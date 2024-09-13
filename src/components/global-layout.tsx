1f346348b99c26e6f852f128428ab450069b8dde
wonke-dev <wonk0311@gmail.com>
2024-09-14 00:28:36 +0900

    글로벌 레이아웃 설정하기

---
+++ src/components/global-layout.tsx
@@ -0,0 +1,16 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+import style from "./global-layout.module.css";
+
+export default function GlobalLayout({ children }: { children: ReactNode }) {
+  return (
+    <div className={style.container}>
+      <header className={style.header}>
+        <Link href={"/"}>📚 ONEBITE BOOKS</Link>
+      </header>
+      <main className={style.main}>{children}</main>
+      <footer className={style.footer}>제작 @wonkyung</footer>
+    </div>
+  );
+}
