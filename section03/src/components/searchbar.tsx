662dbee8954c59f60564aba0f71263f20cda956b
wonke-dev <wonk0311@gmail.com>
2024-10-03 00:36:06 +0900

    네이게이팅

--- section03/src/components/searchbar.tsx
+++ section03/src/components/searchbar.tsx
@@ -1,17 +1,24 @@
 "use client"; // 클라이언트 컴포넌트
 
 import { useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Searchbar() {
+  const router = useRouter();
   const [search, setSearch] = useState("");
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
+
+  const onSubmit = () => {
+    router.push(`/search?q=${search}`);
+  };
+
   return (
     <div>
       <input value={search} onChange={onChangeSearch} />
-      <button>검색</button>
+      <button onClick={onSubmit}>검색</button>
     </div>
   );
 }
