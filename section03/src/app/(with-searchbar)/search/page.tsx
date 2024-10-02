aa0ac82547f9673745b11cb6bd91778d2c0641b2
wonke-dev <wonk0311@gmail.com>
2024-10-03 00:51:29 +0900

    한입북스 UI 구현하기

--- section03/src/app/(with-searchbar)/search/page.tsx
+++ section03/src/app/(with-searchbar)/search/page.tsx
@@ -1,7 +1,18 @@
+import books from "@/mock/books.json";
+import BookItem from "@/components/book-item";
+
 export default function Page({
   searchParams,
 }: {
-  searchParams: { q?: string };
+  searchParams: {
+    q?: string;
+  };
 }) {
-  return <div>Search 페이지 {searchParams.q}</div>;
+  return (
+    <div>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </div>
+  );
 }
--- section03/src/components/searchbar.tsx
+++ section03/src/components/searchbar.tsx
@@ -1,23 +1,42 @@
-"use client"; // 클라이언트 컴포넌트
+"use client";
 
-import { useState } from "react";
-import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import style from "./serachbar.module.css";
 
 export default function Searchbar() {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const [search, setSearch] = useState("");
+
+  const q = searchParams.get("q");
+
+  useEffect(() => {
+    setSearch(q || "");
+  }, [q]);
 
   const onChangeSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearch(e.target.value);
   };
 
   const onSubmit = () => {
+    if (!search || q === search) return;
     router.push(`/search?q=${search}`);
   };
 
+  const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit();
+    }
+  };
+
   return (
-    <div>
-      <input value={search} onChange={onChangeSearch} />
+    <div className={style.container}>
+      <input
+        value={search}
+        onChange={onChangeSearch}
+        onKeyDown={onKeyDown}
+      />
       <button onClick={onSubmit}>검색</button>
     </div>
   );
