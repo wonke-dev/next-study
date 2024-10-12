f9009a6307dca5d6927c7453c12e2a2fe6f4648c
wonke-dev <wonk0311@gmail.com>
2024-10-12 23:00:42 +0900

    클라이언트 컴포넌트에서의 서버액션

--- section07/src/actions/create-review-action.ts
+++ section07/src/actions/create-review-action.ts
@@ -1,17 +1,22 @@
 "use server";
 
+import { delay } from "@/util/delay";
 import { revalidatePath, revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: any, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
   if (!bookId || !content || !author) {
-    return;
+    return {
+      status: false,
+      error: "리뷰 내용과 작성자를 입력해주세요.",
+    };
   }
 
   try {
+    await delay(2000);
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review`,
       {
@@ -19,11 +24,19 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       }
     );
-    console.log(response.status);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
 
     revalidateTag(`review-${bookId}`);
+    return {
+      status: true,
+      error: "",
+    };
   } catch (err) {
-    console.error(err);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${err}`,
+    };
   }
 }
--- section07/src/components/review-editor.tsx
+++ section07/src/components/review-editor.tsx
@@ -1,16 +1,42 @@
+"use client";
+
 import style from "./review-editor.module.css";
 import { createReviewAction } from "@/actions/create-review-action";
+import { use, useActionState, useEffect } from "react";
 
 export function ReviewEditor({ bookId }: { bookId: string }) {
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null
+  );
+
+  useEffect(() => {
+    if (state && !state.status) {
+      alert(state.error);
+    }
+  });
+
   return (
     <section>
-      <form className={style.form_container} action={createReviewAction}>
+      <form className={style.form_container} action={formAction}>
         <input name="bookId" value={bookId} hidden />
 
-        <textarea required name="content" placeholder="리뷰 내용" />
+        <textarea
+          disabled={isPending}
+          required
+          name="content"
+          placeholder="리뷰 내용"
+        />
         <div className={style.submit_container}>
-          <input required name="author" placeholder="작성자" />
-          <button type="submit">작성하기</button>
+          <input
+            disabled={isPending}
+            required
+            name="author"
+            placeholder="작성자"
+          />
+          <button disabled={isPending} type="submit">
+            {isPending ? "..." : "작성하기"}
+          </button>
         </div>
       </form>
     </section>
