ec37ff5d73f8e68e04fd28c67ba57a9e96e097b0
wonke-dev <wonk0311@gmail.com>
2024-10-12 22:43:38 +0900

    태그 기준 데이터 캐시 재검증방법

--- section07/src/actions/create-review-action.ts
+++ section07/src/actions/create-review-action.ts
@@ -1,6 +1,6 @@
 "use server";
 
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 export async function createReviewAction(formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
@@ -20,12 +20,8 @@ export async function createReviewAction(formData: FormData) {
       }
     );
     console.log(response.status);
-    /**
-     * 주의1. 오직 서버측에서만 호출할 수 있는 메서드(클라이언트 컴퍼넌트에서 호출X)
-     * 주의2. 페이지에 포함된 모든 캐시들도 무효화된다.(페이지 재생성)
-     * 주의3. 풀 라우트 캐시까지 함꼐 삭제된다.
-     */
-    revalidatePath(`/book/${bookId}`);
+
+    revalidateTag(`review-${bookId}`);
   } catch (err) {
     console.error(err);
     return;
--- section07/src/app/book/[id]/page.tsx
+++ section07/src/app/book/[id]/page.tsx
@@ -47,7 +47,8 @@ async function BookDetail({ bookId }: { bookId: string }) {
 
 async function ReviewList({ bookId }: { bookId: string }) {
   const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/review/book/${bookId}`,
+    { next: { tags: [`review-${bookId}`] } }
   );
   if (!response.ok) {
     throw new Error(`Review fetch failed:${response.statusText}`);
